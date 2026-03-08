fbb6a555ba9604eb47a6c9eda68dea2fe9448b89
ketkivmohite <ketkimohite214@gmail.com>
2026-03-08 17:47:23 +0530

    fix: remove duplicate URL in main urls.py

--- resume_ai/urls.py
+++ resume_ai/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.resumes.urls")),
-    path("",include("apps.resumes.urls"),)
 ]
 
 if settings.DEBUG:
